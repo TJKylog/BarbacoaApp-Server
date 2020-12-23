b478bc4c3411e9426ce8de7ddee0cb7b1061c5c4
TJKylog <jesuus.23@gmail.com>
2020-12-23 12:55:31 -0600

    New chanes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 use App\User;
 use Illuminate\Support\Str;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
