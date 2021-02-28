bca923e7df7f464f2e2ed9b8ab10b8106c514384
TJKylog <jesuus.23@gmail.com>
2021-02-28 14:26:04 -0600

    Bug fix create user

--- app/Event.php
+++ app/Event.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = [
-        'status', 'event_info',
+        'is_conplete', 'event_info',
     ];
 
     protected $casts = [
